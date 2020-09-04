0b14286310fd327433e5bb8c586f9712aee02db3
Jun0zo <37208901+Jun0zo@users.noreply.github.com>
2020-09-04 15:20:33 +0900

    ver 0.3
    add notice "새로운 자비스 준비중입니다.
    <<M-자비스 마케팅 커뮤니티>>
    https://cafe.naver.com/mjarvis2080"

--- app.js
+++ app.js
@@ -1,40 +1,42 @@
 // http://no1sense1.dothome.co.kr/
+const express = require('express')
+const app     = express()
+const scraper = require('./my_modules/scraper')
+const path    = require('path')
 
-const express = require("express");
-const app = express();
-const scraper = require("./my_modules/scraper");
-const path = require("path");
+const notice = "—————————————\n\n🙇🏻 새로운 자비스 준비중입니다.\n<<M-자비스 마케팅 커뮤니티>>\nhttps://cafe.naver.com/mjarvis2080"
 
-app.use(express.json()); //body-parser
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json()) //body-parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get("/keyboard", (req, res, next) => {
-  res.json({
-    type: "buttons",
-    buttons: ["자비스 키워드 조회"]
-  });
-});
+app.get('/keyboard', (req, res, next) => {
+    res.json({
+        'type': 'buttons',
+        'buttons': ['자비스 키워드 조회']
+    })
+})
 
-app.post("/message", (req, res, next) => {
-  const command = req.body.userRequest.utterance;
+app.post('/message', (req, res, next) => {
+    const command = req.body.content
 
-  scraper.scrape(command).then(result => {
-    const responseBody = {
-      contents: [
-        {
-          type: "text",
-          text: result
+    scraper.scrape(command).then(result => {
+        const responseBody = {
+            'message': {
+                'text': result
+            }
         }
-      ]
-    };
-    res.status(200).send(responseBody);
-  });
-});
+        res.status(200).send(responseBody)
+    })
+})
+
+var server = app.listen(4000, function(){
+    console.log("Running...!!");
+})
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  res.send("404 Not found").status(404);
-});
+    res.send('404 Not found').status(404)
+})
 
-module.exports = app;
+module.exports = app
